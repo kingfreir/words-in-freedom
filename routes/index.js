783d26fd5f1b149b767671ea10fa443c03bead9b
Ricardo Rodrigues <2080414@student.uma.pt>
2018-04-05 12:11:48 +0100

    Clear old files

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var controller = require('../controllers/index.js');
-
-router.get('/',controller.default);
-
-module.exports = router;
--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-var path = require('path')
-
-module.exports = {
-  entry: './client/js/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public/js')
-  }
-};
