73e31937e82e8f073bcbe71b26d14ea1ef73df78
Ricardo Rodrigues <2080414@student.uma.pt>
2018-04-07 18:13:31 +0100

    Add webpack and babel

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import Home from './scenes/Home/Home'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<Home />, document.getElementById('root'))
